787ab05ea9b116bc752246955cfd0cdf2e9e93a0
Spdearing <142638816+Spdearing@users.noreply.github.com>
2023-09-23 18:04:52 -0400

    Implimented Basic Settings Menu, for Sound, Quality, Full Screen, and Resolution

--- root/Team2Project2/Assets/Scripts/UI/SettingsMenu.cs
+++ root/Team2Project2/Assets/Scripts/UI/SettingsMenu.cs
@@ -3,13 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
 using UnityEngine.UI;
+using TMPro;
 
 public class SettingsMenu : MonoBehaviour
 {
     [SerializeField] private AudioMixer audioMixer;
-    [SerializeField] private Resolution[] resolutions;
-    public Dropdown resolutionDropdown;
-    
+
+    [SerializeField] private TMP_Dropdown resolutionDropdown;
+
+    Resolution[] resolutions;
+
 
 
     void Start()
@@ -20,13 +23,21 @@ public class SettingsMenu : MonoBehaviour
 
         List<string> options = new List<string>();
 
+        int currentResolutionIndex = 0;
         for(int i = 0; i <resolutions.Length; i++)
         {
             string option = resolutions[i].width + "x" + resolutions[i].height;
             options.Add(option);
+
+            if(resolutions[i].width == Screen.currentResolution.width && resolutions[i].height == Screen.currentResolution.height)
+            {
+                currentResolutionIndex = i;
+            }
         }
 
         resolutionDropdown.AddOptions(options);
+        resolutionDropdown.value = currentResolutionIndex;
+        resolutionDropdown.RefreshShownValue();
     }
 
     public void SetVolume(float volume)
@@ -42,5 +53,11 @@ public class SettingsMenu : MonoBehaviour
     public void SetFullScreen(bool isFullScreen)
     {
         Screen.fullScreen = isFullScreen;
+    }
+
+    public void SetResolution(int resolutionIndex)
+    {
+        Resolution resolution = resolutions[resolutionIndex];
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
     }
 }
