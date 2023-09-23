f2be1c841dcfeabdc7211c9aa9a88d2a902ed7a0
zebbaukhagen <69488628+zebbaukhagen@users.noreply.github.com>
2023-09-23 18:05:21 -0400

    finally fucking fixed the player controller

--- root/Team2Project2/Assets/Scripts/Player/PlayerController.cs
+++ root/Team2Project2/Assets/Scripts/Player/PlayerController.cs
@@ -4,45 +4,36 @@ public class PlayerController : MonoBehaviour
 {
     public float speed = 5f;  // Movement speed
     public float rotationSpeed = 0.15f;  // Speed of rotation
-
+    public float inputThreshold = 0.1f;  // Threshold for input to be considered significant
+    private Vector3 movement;
     private Rigidbody rb;
 
-    // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
-        // Get the Rigidbody component attached to the player GameObject
         rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        // Read input from WASD keys
-        float horizontalInput = Input.GetAxis("Horizontal");
-        float verticalInput = Input.GetAxis("Vertical");
-
-        // Calculate the movement vector
-        Vector3 movement = new Vector3(horizontalInput, 0f, verticalInput);
-
-        // Rotate the player to face the direction of movement
-        if (movement != Vector3.zero)
-        {
-            Quaternion desiredRotation = Quaternion.LookRotation(movement);
-            transform.rotation = Quaternion.Slerp(transform.rotation, desiredRotation, rotationSpeed);
-        }
-    }
-
-    // FixedUpdate is called once per physics frame
     void FixedUpdate()
     {
         // Read input from WASD keys
         float horizontalInput = Input.GetAxis("Horizontal");
         float verticalInput = Input.GetAxis("Vertical");
+        rb.angularVelocity = Vector3.zero;
 
-        // Calculate the movement vector
-        Vector3 movement = new Vector3(horizontalInput, 0f, verticalInput);
+        // Check if the input is above the threshold
+        if (Mathf.Abs(horizontalInput) > inputThreshold || Mathf.Abs(verticalInput) > inputThreshold)
+        {
+            // Calculate the movement vector
+            movement = new Vector3(horizontalInput, 0f, verticalInput);
+            if (movement != Vector3.zero)
+            {
+                Quaternion desiredRotation = Quaternion.LookRotation(movement);
+                rb.rotation = Quaternion.Lerp(transform.rotation, desiredRotation, rotationSpeed);
 
-        // Move the player using Rigidbody
-        rb.MovePosition(rb.position + movement * speed * Time.fixedDeltaTime);
+                // Move the player
+                rb.MovePosition(rb.position + speed * Time.deltaTime * movement);
+            }
+        }
     }
 }
