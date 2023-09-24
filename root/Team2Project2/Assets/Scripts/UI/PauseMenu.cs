5b337ddbe8ace56508ad7e396784b673a6195522
Spdearing <142638816+Spdearing@users.noreply.github.com>
2023-09-24 17:03:54 -0400

    Pause menu almost finished need to resize buttons on Pause Menu Settings

--- root/Team2Project2/Assets/Scripts/UI/PauseMenu.cs
+++ root/Team2Project2/Assets/Scripts/UI/PauseMenu.cs
@@ -1,20 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class PauseMenu : MonoBehaviour
 {
     //[SerializeField] private SceneLoader sceneLoader;
-    [SerializeField] private PlayerController playerController;
+    //[SerializeField] private PlayerController playerController;
     [SerializeField] private GameObject pauseMenu;
-   
-     //[SerializeField] private static bool backToMainMenu = false;
-     [SerializeField] private static bool togglePauseGame;
+    [SerializeField] private GameObject settingsMenu;
+    [SerializeField] private TMP_Text levelPrompt;
+
+    //[SerializeField] private static bool backToMainMenu = false;
+    [SerializeField] private static bool togglePauseGame;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        settingsMenu.SetActive(false);
     }
 
     // Update is called once per frame
@@ -38,23 +43,45 @@ public class PauseMenu : MonoBehaviour
         if (togglePauseGame == true)
         {
             pauseMenu.SetActive(true);
-            playerController.enabled = false;
+            //playerController.enabled = false;
             Time.timeScale = 0f;
+            if(SceneManager.GetActiveScene().name == "MainMenu")
+            {
+                levelPrompt.text = "THIS IS A TEST!";
+            }
+            //else if(SceneManager.GetActiveScene().name == "LevelOne")
+            //{
+            //    levelPrompt.text = "Level 1: Escape The Flames! ";
+            //}
+            //if(SceneManager.GetActiveScene().name == "LevelTwo")
+            //{
+            //    levelPrompt.text = "Level 2: Connect The Conduits! ";
+            //}
+            //else if (SceneManager.GetActiveScene().name == "LevelThree")
+            //{
+            //    levelPrompt.text = "Level 1: Escape The Roomba! ";
+            //}
         }
         else
         {
             pauseMenu.SetActive(false);
-            playerController.enabled = true;
+            //playerController.enabled = true;
             Time.timeScale = 1f;
         }
+    }
+
+    public void OpenSettings()
+    {
+        settingsMenu.SetActive(!settingsMenu.activeSelf);
     }
 
     private void BackToMainMenu()
     {
         //sceneLoader.LoadScene(0);
         pauseMenu.SetActive(false);
-        playerController.enabled = false;
+       // playerController.enabled = false;
         Cursor.lockState = CursorLockMode.None;
         Time.timeScale = 0f;
     }
+
 }
