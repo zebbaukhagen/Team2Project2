4594e2eb64c65fa58a364d0bc1cd925e00efc37a
Spdearing <142638816+Spdearing@users.noreply.github.com>
2023-10-07 15:46:07 -0400

    Instructions Menu updated, Level Selector not implemented yet

--- root/Team2Project2/Assets/Scripts/UI/InstructionsPanel.cs
+++ root/Team2Project2/Assets/Scripts/UI/InstructionsPanel.cs
@@ -14,13 +14,13 @@ public class InstructionsPanel : MonoBehaviour
     [SerializeField] private TMP_Text levelTitleText;
     [SerializeField] private TMP_Text levelDetails;
     [SerializeField] private UIMainMenuController levelJump;
-    private Renderer renderer;
+
 
     // Start is called before the first frame update
     void Start()
     {
         LevelOneSelected();
-        renderer = GetComponent<Renderer>();    
+   
     }
 
     // Update is called once per frame
@@ -31,23 +31,21 @@ public class InstructionsPanel : MonoBehaviour
 
     public void LevelOneSelected()
     {
-        renderer.material.color = Color.black;
         levelTitleText.text = " Level One: Escape the Flames";
         levelDetails.text = "Complete the following binary puzzles to turn off the fires that are blocking the doors and make it to the exit to progress to Level Two";
     }
 
     public void LevelTwoSelected()
     {
-        renderer.material.color = Color.black;
+       
         levelTitleText.text = " Level Two: Connect the Conduits";
         levelDetails.text = "Complete the electrical current to unlock the main door, and progress to Level Three";
     }
 
     public void LevelThreeSelected() 
     {
-        renderer.material.color = Color.black;
         levelTitleText.text = " Level Three: Roombas are hungry!";
-        levelDetails.text = "Maneuver through the maze, collecting fuse pickups that are needed to unlock the Scientist Lab, then extract revenge on your abuser ";
+        levelDetails.text = "Maneuver through the maze, collecting fuse pickups that are needed to unlock the Scientist Lab. Avoid the Roombas, make your escape, then extract revenge on your abuser ";
     }
 }
 
